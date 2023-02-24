2e87b632624ee5a932567161781bffd37af702dc
arbaz <arbajtamboli1@gmail.com>
2023-02-24 10:06:03 +0530

    Add project files.

---
+++ EmployeeManagement/Data/MVCDemoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class MVCDemoDBContext : DbContext
+    {
+        public MVCDemoDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
